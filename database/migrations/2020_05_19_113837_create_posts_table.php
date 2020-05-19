5dbab3208ad469f534e8296338b38846e0ec8706
Luca <lu.cucchi@gmail.com>
2020-05-19 14:02:34 +0200

    Created DB + DB migration + Model + Controller

---
+++ database/migrations/2020_05_19_113837_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->boolean('published', 1);
+            $table->string('title', 150);
+            $table->string('author', 80);
+            $table->text('body');
+            $table->text('img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
