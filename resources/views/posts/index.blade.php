c71cac917b79db241c213e5df46cd778b1b8ab60
Luca <lu.cucchi@gmail.com>
2020-05-20 16:02:46 +0200

    fix

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,9 @@
 <ul>
     @foreach ($posts as $post)
-        <li><h2>{{$post->title}}</h2></li>
+        <li><a href="{{route('posts.show', $post->slug)}}"><h2>{{$post->title}}</h2></a></li>
         <li><small>Autore: {{$post->author}}</small></li>
-        <li><img src="{{$post->img}}" alt="foto"></li>
         <li><p>{{$post->body}}</p></li>
+        <li><img src="{{$post->img}}" alt="foto"></li>
         <li>Created: {{$post->created_at}}</li>
     @endforeach
 </ul>
