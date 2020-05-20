263c58b3fca70fb816f7fe9c7cd2e4c0b4c24f6e
Luca <lu.cucchi@gmail.com>
2020-05-20 16:59:16 +0200

    fix style

--- resources/views/posts/published.blade.php
+++ resources/views/posts/published.blade.php
@@ -1,9 +1,31 @@
-<ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+<body>
     @foreach ($published as $post)
-        <li><h2>{{$post->title}}</h2></li>
-        <li><small>Autore: {{$post->author}}</small></li>
-        <li><img src="{{$post->img}}" alt="foto"></li>
-        <li><p>{{$post->body}}</p></li>
-        <li>Created: {{$post->created_at}}</li>
+        <div class="container col-11">
+            <div class="row">
+                <div>
+                    <a href="{{route('posts.show', $post->slug)}}"><h2>{{$post->title}}</h2></a>
+                    <small>Autore: {{$post->author}}</small>
+                    <div>
+                        <small>Created: {{$post->created_at}}</small>
+                    </div>
+                    <div class="col-4 offset-4">
+                        <img src="{{$post->img}}" alt="foto">
+                    </div>
+                    <p>{{$post->body}}</p>
+
+
+                </div>
+            </div>
+        </div>
     @endforeach
-</ul>
+</body>
+</html>
